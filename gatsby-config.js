2d2c9f1afa0af3591bfb2b532ca53f309f4c99c7
Alec <acaputo@mail.smcvt.edu>
2019-02-23 16:46:48 -0500

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,21 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portfolio`,
+        short_name: `Portfolio`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/alec.jpg`, // This path is relative to the root of the site.
+        include_favicon: true // Include favicon
+      },
+    },
+    {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })]
